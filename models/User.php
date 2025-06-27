07f9cecf9309d376af812215ee38eab99ab4c9db
Danny <dhayek92@gmail.com>
2025-06-27 16:09:56 +0300

    Added password as an attribute in User model to be fetched from db

--- models/User.php
+++ models/User.php
@@ -9,6 +9,7 @@ class User extends Model {
     private string $name;
     private string $email;
     private string $phone_number;
+    private string $password;
 
     public function __construct(array $data)
     {
@@ -16,9 +17,10 @@ class User extends Model {
         $this->name = $data["name"];
         $this->email = $data["email"];
         $this->phone_number = $data["phone_number"];
+        $this->password = $data["password"];
     }
 
     public function toArray() {
-        return [$this->id, $this->name, $this->email, $this->phone_number];
+        return [$this->id, $this->name, $this->email, $this->phone_number, $this->password];
     }
 }
