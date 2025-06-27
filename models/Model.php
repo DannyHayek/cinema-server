e61c8f6e5670e3520a14dca0902eceffa83f70ca
Danny <dhayek92@gmail.com>
2025-06-27 13:04:55 +0300

    Created model.php superclass to serve as foundation for other models with select and selectAll functions

---
+++ models/Model.php
@@ -0,0 +1,35 @@
+<?php
+
+abstract class Model {
+    protected static string $table;
+    protected static string $primary_key = "id";
+
+    public static function select (mysqli $mysqli, int $id) {
+        $sql = sprintf(("SELECT * FROM %s WHERE %s = ?"), static::$table, static::$primary_key);
+
+        $query = $mysqli->prepare($sql);
+        $query->bind_param("i", $id);
+        $query->execute();
+
+        $data = $query->get_result()->fetch_assoc();
+
+        return $data ? new static($data) : null;
+    }
+
+    public static function selectAll (mysqli $mysqli) {
+        $sql = sprintf(("SELECT * FROM %s"), static::$table);
+
+        $query = $mysqli->prepare($sql);
+        $query->execute();
+
+        $data = $query->get_result();
+        $objects = [];
+
+        while ($row = $data->fetch_assoc()) {
+            $objects[] = new static($row);
+        }
+
+        return $objects;
+    }
+}
+
