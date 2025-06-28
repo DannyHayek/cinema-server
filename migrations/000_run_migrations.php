fe66ad82542c2c92ebf66c66899c626cf2d8acbf
Danny <dhayek92@gmail.com>
2025-06-28 13:01:15 +0300

    Cleaned up migrations and fixed order

---
+++ migrations/000_run_migrations.php
@@ -0,0 +1,24 @@
+<?php
+require "../connection/connection.php";
+
+// try {
+//     $query = "CREATE TABLE users(
+//           id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+//           name VARCHAR(255) NOT NULL, 
+//           email VARCHAR(255) NOT NULL, 
+//           phone_number VARCHAR(255) NOT NULL, 
+//           password VARCHAR(255) NOT NULL, 
+//           age INT (3),
+//           favorite_genre_id INT(11), 
+//           FOREIGN KEY (favorite_genre_id) REFERENCES genres(id) 
+//           )";
+
+// $execute = $mysqli->prepare($query);
+// $execute->execute();
+
+// echo "Users table succesfully created";
+
+// } catch(Throwable $e) {
+//     echo $e;
+// }
+
