2f8eac0ed864c16a94196007e0d9957a8319befd
Danny <dhayek92@gmail.com>
2025-06-28 11:31:02 +0300

    Created migration for movies table

---
+++ migrations/002_create_movies_table.php
@@ -0,0 +1,23 @@
+<?php
+require "../connection/connection.php";
+
+try {
+    $query = "CREATE TABLE movies(
+          id INT(11) AUTO_INCREMENT PRIMARY KEY, 
+          name VARCHAR(255) NOT NULL, 
+          synopsis TEXT(1000) NOT NULL, 
+          cast TEXT(255) NOT NULL, 
+          length INT(11) NOT NULL, 
+          rating INT(2) NOT NULL, 
+          trailer_link VARCHAR(225) 
+          )";
+
+$execute = $mysqli->prepare($query);
+$execute->execute();
+
+echo "Movies table succesfully created";
+
+} catch(Throwable $e) {
+    echo $e;
+}
+
