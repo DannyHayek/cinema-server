1f305e30197da6164a53ad2af290008eee8ebeb4
Danny <dhayek92@gmail.com>
2025-07-06 16:41:01 +0300

    Created routes folder and file with login and register basic temp routes

---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+$apis = [
+    '/login'         => ['controller' => 'AuthController', 'method' => 'login'],
+    '/signup'         => ['controller' => 'AuthController', 'method' => 'signup'],
+];
+
